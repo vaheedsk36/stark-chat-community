a3cd52ce601aaa9b5a92d0b6a947a4a7ecbc9584
MASOOD AKHTAR VAHEED <masoodakhtarvaheed@MASOODs-MacBook-Air.local>
2023-04-07 01:06:42 +0530

    phase-1-stark-chat-community-app-completed

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const path = require('path');
 const socketio = require('socket.io');
 const formatMessage = require('./utils/messages');
-const {userJoin,getCurrentUser} = require('./utils/users');
+const {userJoin,getCurrentUser,userLeave,getRoomUsers} = require('./utils/users');
 
 const app = express();
 const server = http.createServer(app);
@@ -20,26 +20,43 @@ const botName = 'Chat Cord Bot';
 io.on('connection',socket=>{
 
     socket.on('joinRoom',({username,room})=>{
+        const user = userJoin(socket.id,username,room);
+        socket.join(user.room);
 
          // emit to the user connect
     socket.emit('message', formatMessage(botName, 'Welcome to ChatCord!'));
     
     // emit to everyone when a user connects
-    socket.broadcast.emit('message',formatMessage(botName, 'A user has joined the chat'));
+    socket.broadcast.to(user.room).emit('message',formatMessage(user.username, `A ${user.username} has joined the chat`));
 
+    // Send users in the room
+    io.to(user.room).emit('roomUsers',{
+        room: user.room,
+        users: getRoomUsers(user.room)
+    })
 
     });
    
-    // Runs when a client disconnect
-    socket.on('disconnect',()=>{
-        io.emit('message',formatMessage(botName,'A user has left the chat'));
-    });
-
     // Listen for chatMessage
 
     socket.on('chatMessage',msg=>{
-        io.emit('message',formatMessage('USER',msg));
+        const user = getCurrentUser(socket.id);
+        io.to(user.room).emit('message',formatMessage(`${user.username}`,msg));
     })
+
+    // Runs when a client disconnect
+    socket.on('disconnect',()=>{
+        const user = userLeave(socket.id);
+
+        if(user){
+            io.to(user.room).emit('message',formatMessage(botName,`${user.username} has left the chat`));
+                // Send users in the room
+    io.to(user.room).emit('roomUsers',{
+        room: user.room,
+        users: getRoomUsers(user.room)
+    })
+        }
+    });
 });
 
 server.listen(PORT,()=>console.log(`Server running on ${PORT}`));
